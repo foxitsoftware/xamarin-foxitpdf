d99a9ba12bde1094c2130e52215621e5ed37b2c7
zhiqiang_ye <zhiqiang_ye@foxitsoftware.com>
2023-09-01 10:54:38 +0800

    update code

--- foxit_xamarin_ios/CompletePdfViewer/AppDelegate.cs
+++ foxit_xamarin_ios/CompletePdfViewer/AppDelegate.cs
@@ -130,20 +130,39 @@ namespace CompletePdfViewer
             }
             else
             {
-#if !NET
-                UIStoryboard storyboard = UIStoryboard.FromName("Main", NSBundle.MainBundle);
-                var vc = storyboard.InstantiateInitialViewController();
-#else
+#if NET
                 var vc = new ViewController();
-#endif
+
                 Window.RootViewController = vc;
 
-                // make the window visible
+                //make the window visible
                 Window.MakeKeyAndVisible();
+#endif
             }
 
             return true;
         }
+
+
+#if !NET
+        // UISceneSession Lifecycle
+
+        [Export("application:configurationForConnectingSceneSession:options:")]
+        public UISceneConfiguration GetConfiguration(UIApplication application, UISceneSession connectingSceneSession, UISceneConnectionOptions options)
+        {
+            // Called when a new scene session is being created.
+            // Use this method to select a configuration to create the new scene with.
+            return UISceneConfiguration.Create("Default Configuration", connectingSceneSession.Role);
+        }
+
+        [Export("application:didDiscardSceneSessions:")]
+        public void DidDiscardSceneSessions(UIApplication application, NSSet<UISceneSession> sceneSessions)
+        {
+            // Called when the user discards a scene session.
+            // If any sessions were discarded while the application was not running, this will be called shortly after `FinishedLaunching`.
+            // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        }
+#endif
     }
 
 }
